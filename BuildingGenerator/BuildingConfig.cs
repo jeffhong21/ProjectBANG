be444e6432dbfb5cb6dd70e2cf177baf769cd838
Jeffrey Hong <jeffhong21@gmail.com>
2019-06-16 19:37:35 -0700

    building generator

---
+++ BuildingGenerator/BuildingConfig.cs
@@ -0,0 +1,17 @@
+namespace BuildingGenerator
+{
+    using UnityEngine;
+
+
+    public class BuildingConfig : MonoBehaviour
+    {
+        public bool isBalcony;
+        public bool isDeckCover;
+        public bool isStairs;
+        public bool hasDoor;
+
+
+    }
+}
+
+
