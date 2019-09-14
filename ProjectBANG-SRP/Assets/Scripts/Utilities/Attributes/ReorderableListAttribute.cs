5b85548ee446495af4e02be737e7a9572ab2d5ca
Jeffrey Hong <>
2019-09-14 00:13:01 -0700

    cleanup

--- ProjectBANG-SRP/Assets/Scripts/Utilities/Attributes/ReorderableListAttribute.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using System;
-
-[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
-public class ReorderableListAttribute : PropertyAttribute
-{
-}
